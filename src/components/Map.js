8cf8ee202a0e19225a93c14f0b2cda63f622d846
Elmankyau <alamelman@gmail.com>
2023-02-03 16:55:20 +0600

    Update Marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,60 +1,57 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-import {Marker} from 'react-leaflet'
+import React from "react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 
 const containerStyle = {
-  width: '95%',
-  height: '550px'
+  width: "95%",
+  height: "550px",
 };
 
-const center = {
-  lat: 23.8103,
+const center = { 
+  lat: 23.8103, 
   lng: 90.4125
-};
-
+ }
 
 function Map(props) {
   const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyB16nQHoNGmnVfRM8q6Ha2AXowviSHTdgo"
-    
-  })
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyB16nQHoNGmnVfRM8q6Ha2AXowviSHTdgo",
+  });
 
-  const [map, setMap] = React.useState(null)
+  const [map, setMap] = React.useState(null);
 
   const onLoad = React.useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     // const bounds = new window.google.maps.LatLngBounds(center);
     const bounds = new window.google.maps.LatLngBounds({
       lat: props.lat,
-      lng: props.lng
+      lng: props.lng,
     });
-    <Marker position={[props.lat, props.lng]}></Marker>
 
     map.fitBounds(bounds);
 
-    setMap(map)
-  }, [])
+    setMap(map);
+  }, []);
 
   const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+    setMap(null);
+  }, []);
 
   return isLoaded ? (
-    
-      <GoogleMap
-      
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={20}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={20}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      <Marker position={center}/>
+      <></>
+      {}
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 }
 
-export default Map
+export default Map;
